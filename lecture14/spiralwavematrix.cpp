8f2dcb190ba8ab724ec3ce43cfe2146530a8fea2
surk2025 <surjeetkushwaha1847@gmail.com>
2026-06-15 21:31:18 +0530

    spiral wave matrix

--- lecture14/spiralwavematrix.cpp
+++ lecture14/spiralwavematrix.cpp
@@ -8,18 +8,31 @@ int main(){
     int m;
     cout<<"enter coloum: ";
     cin>>m;
-    int arr[m][n];
+    int arr[n][m];
     cout<<"Input: "<<endl;
-    for(int i=0;i<m;i++){
-        for(int j=0;j<n;j++){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
             cin>>arr[i][j];
         }    
     }
-    
-    cout<<"Spiral Matrix: "<<endl;
-    for(int i=0;i<m;i++){
-        for(int j=0;j<n;j++){
-            cout<<arr[i][j]<<" ";
-        } 
+    int minr=0,minc=0;
+    int maxr=n-1,maxc=m-1;
+    while(minr<=maxr && minc<=maxc){
+        for(int j=minc;j<=maxc;j++){
+            cout<<arr[minr][j]<<" ";
+        }
+        minr++;
+        for(int i=minr;i<=maxr;i++){
+            cout<<arr[i][maxc]<<" ";
+        }
+        maxc--;
+        for(int j=maxc;j>=minc;j--){
+            cout<<arr[maxr][j]<<" ";
+        }
+        maxr--;
+        for(int i=maxr;i>=minr;i--){
+            cout<<arr[i][minc]<<" ";
+        }
+        minc++;
     }
 }
